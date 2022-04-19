82eedb54b7eea4d203a09e104871787f47e9fae4
arunz1995 <arunyesodharan2@gmail.com>
2022-04-19 11:23:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+     <div className="login">
+        <div className="login-section">
+          <div className="login-header">
+       <h1> log IN </h1>
+       
+          </div>
+       <input type="text" placeholder="username" id='name' />
+      <br/>
+        <input type="password" placeholder="password" id='password' />
+        <br/>
+        </div>
+        <div className="login-button">
+          <button> 
+          
+             </button>
+         
+        </div>
+        <br/>
+        <br/>
+        <p>"connect people around"</p>
+     </div>
+      <div className="signup">
+      
+      </div>
     </div>
   );
 }
