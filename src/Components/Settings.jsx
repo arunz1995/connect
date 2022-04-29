d3358cd1178d851047054fe38f87f59e8dfb52fd
arunz1995 <arunyesodharan2@gmail.com>
2022-04-29 12:45:55 +0530

    commit

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -5,6 +5,16 @@ const Settings = () => {
   return (
     <div className="settings-container">
         <div className="settings-body">
+            <h1>Settings</h1>
+            
+            <div className="settings-body-content">
+            <button className="settings-button">Edit Profile</button>
+            <button className="settings-button">Privacy & Security</button>
+            <button className="settings-button">Blocked Accounts</button>
+            <button className="settings-button">Privacy Policy & Community Guidelines</button>
+            <button className="settings-button">Log Out</button>
+            </div>
+
             </div>
     </div>
   )
